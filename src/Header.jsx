f03d7b4a73d9a26accf7adc1eac2ad55dd65b2b4
devsivv <shivamdubey9040@gmail.com>
2026-01-07 20:00:12 +0530

    check

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,7 +2,7 @@ function Header() {
     return (
         <header className="header">
             <h1>
-                teja mc hai
+                Weather Dashboard
             </h1>
         </header>
     );
